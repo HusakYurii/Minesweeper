025b99b30841a5a78b3179783ba908e1bc60fc9f
Yurii Husak <gusak.1881@gmail.com>
2019-11-09 17:33:38 +0200

    game config was cleared and updated

--- src/configs/gameConfig.js
+++ src/configs/gameConfig.js
@@ -3,6 +3,7 @@
   * otherwise grid will be set as default 9 by 9 with 10 mines on it */
 export default {
   "name": "Minesweeper",
+  "debuggerMode": true,
   "defaultGrid": {
     "minesAmount": 10,
     "columns": 9,
@@ -23,34 +24,10 @@ export default {
   ],
   "popup": {
     "winPopup": {
-      "text": "Oh! You lose!",
-      "styles": {
-        "fill": "#150343",
-        "fontSize": 45,
-        "fontWeight": "bold"
-      },
-      "ui": {
-        "type": "RoundedRect",
-        "width": 700,
-        "height": 400,
-        "corner": 25,
-        "color": "#dae0e7"
-      }
+
     },
     "losePopup": {
-      "text": "Well done! You win!",
-      "styles": {
-        "fill": "#150343",
-        "fontSize": 45,
-        "fontWeight": "bold"
-      },
-      "ui": {
-        "type": "RoundedRect",
-        "width": 700,
-        "height": 400,
-        "corner": 25,
-        "color": "#dae0e7"
-      }
+
     }
   }
 

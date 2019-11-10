b2da03ff40d367655cc15770af795e32021283cd
Yurii Husak <gusak.1881@gmail.com>
2019-11-10 13:48:36 +0200

    Game config was updated. Model was refactored

--- src/configs/gameConfig.js
+++ src/configs/gameConfig.js
@@ -4,8 +4,9 @@
 export default {
   "name": "Minesweeper",
   "debuggerMode": true,
-  "setGridManually": true,
-  "gridSize": 700, // basically it is a with of a square
+  "setGridManually": false,
+  // basically it is a with of a square
+  "gridSize": 700,
   "defaultGrid": {
     "minesAmount": 10,
     "columns": 9,
@@ -23,7 +24,11 @@ export default {
     [ 0, 0,-1, 0, 0, 0, 0, 0, 0, 0 ],
     [-1, 0, 0, 0, 0, -1, 0, 0, 0, 0 ]
   ],
-
+  // applicable only for mobile devices, time in `ms`
+  "viewTimingData": {
+    "flagRequestTimeout": 200,
+    "doubleTapTimeout": 100
+  },
   "popup": {
     "winPopup": {
 
--- src/game/model/index.js
+++ src/game/model/index.js
@@ -2,6 +2,7 @@ import CellModel from "./cellModel";
 
 export default class Model {
   constructor() {
+    this.viewTimingData = null;
     this.originalData = null;
     this.grid = {
       rows: 0,
@@ -11,8 +12,31 @@ export default class Model {
     };
   }
 
-  parseConfig(config) {
+  /** To set and parse data
+   * @param {Object} config - original config data
+   * @param {Array<Array>} initializedMap - map which is prepared by Engine */
+  addData(config, initializedMap) {
+    this.viewTimingData = config.viewTimingData;
     this.originalData = config;
 
+    this.grid = {
+      gridSize: config.gridSize,
+      rows: initializedMap.length,
+      columns: initializedMap[ 0 ].length,
+      map: this.convertMap(initializedMap)
+    };
+  }
+
+  /** To convert raw map data into structured data for cells
+   * @param {Array<Array>} initializedMap
+   * @return {Array<CellModel>} */
+  convertMap(initializedMap) {
+    return initializedMap.map((row, rowIndx) => {
+      return row.map((val, colIndx) => {
+        const isMine = val < 0;
+        const text = ( isMine || val === 0 ) ? "" : String(val);
+        return new CellModel(isMine, text, rowIndx, colIndx);
+      });
+    });
   }
 }

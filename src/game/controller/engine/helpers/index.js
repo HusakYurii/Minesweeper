1c7fb0dcd44ce02ac6dab2b0474b1a2136740879
Yurii Husak <gusak.1881@gmail.com>
2019-11-10 16:25:35 +0200

    a helper was added to check if a cell is revealed

--- src/game/controller/engine/helpers/index.js
+++ src/game/controller/engine/helpers/index.js
@@ -11,8 +11,13 @@ const isMine = (arr, row, col) => {
   return !!Number(cell) ? cell < 0 : cell.isMine;
 };
 
+const isRevealed = (arr, row, col) => {
+  return arr[ row ][ col ].isRevealed;
+};
+
 export {
   getRndInt,
+  isRevealed,
   isValid,
   isMine
 }
